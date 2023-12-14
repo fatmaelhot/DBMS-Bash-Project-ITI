e57c55864e244301f673999e2efbcf317f48c728
fatmaelhot <fatmaelhot289@gmail.com>
2023-12-14 15:41:46 +0200

    update

---
+++ main.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+
+
+# Create a database directory if it doesn't exist
+[[ -d ./DataBase ]] || mkdir ./DataBase
+
+while true; do
+  PS3="Welcome to BDMS Main Menu: Please Select an option (1-5): "
+
+select var in createDB listDB dropDB connectDB Exit
+do
+	case $var in
+		createDB )	
+
+			 ./CreateDB.sh
+		;;
+		listDB )
+			./listDB.sh
+		;;
+		dropDB ) 
+		         ./dropdb.sh
+		;;
+
+		connectDB )
+			 ./connectdb.sh
+
+			 
+		;;
+		listDB )
+			
+		;;
+		dropDB ) 
+		        bash ./dbmenu/dropdb.sh
+		;;
+
+		connectDB )
+			bash ./dbmenu/connectdb.sh
+
+		;;
+		Exit )
+		   exit
+		;;
+		* ) 
+		     echo "$REPLY is incorrect input";
+		     exit
+		;;
+	esac
+
+break
+
+done
+done
+=======
+
+done
+>>>>>>> 146de23be336fa3486f0fd085091a1572ce6fd2e
