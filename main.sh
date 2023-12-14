76b1a266a2a6774f8a99d3128e4bde776ea3a76b
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2023-12-14 21:47:02 +0200

    merge files

--- main.sh
+++ main.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/bash
+
+
+# Create a database directory if it doesn't exist
+[[ -d ./DBs ]] || mkdir ./DBs
+
+while true; do
+  PS3="Welcome to BDMS Main Menu: Please Select an option (1-5): "
+
 select var in createDB listDB dropDB connectDB Exit
 do
 	case $var in
 		createDB )	
-			 
+
+			 ./dbmenu/createdb.sh
 		;;
 		listDB )
-			
+			./dbmenu/listdb.sh
 		;;
 		dropDB ) 
-		        bash ./dbmenu/dropdb.sh
+		         ./dbmenu/dropdb.sh
 		;;
 
 		connectDB )
-			bash ./dbmenu/connectdb.sh
+			 ./dbmenu/connectdb.sh	 
 		;;
+		 
 		Exit )
 		   exit
 		;;
@@ -24,4 +34,10 @@ do
 		;;
 	esac
 
+break
+
 done
+done
+
+
+
